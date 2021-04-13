254f6a4241dac5c1217e7dd548430cb88ed18093
MPJ <merrilpious@gmail.com>
2021-04-13 09:37:44 +0530

    Update siteConfig.js

--- siteConfig.js
+++ siteConfig.js
@@ -1,6 +1,6 @@
 module.exports = {
     // Do not include a trailing slash!
-    siteUrl: `https://demo.jamify.org`,
+    siteUrl: `https://loving-ritchie-25956d.netlify.app`,
 
     // Enable infinite scroll (default: true)
     infiniteScroll: true,
